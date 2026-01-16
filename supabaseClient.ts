808927e7a63343a1867e5558ce03fcbf5e7eb62e
appestoicismo <contato@appestoicismo.com.br>
2026-01-16 20:34:50 +0000

    Chave Anon Oficial instalada

--- supabaseClient.ts
+++ supabaseClient.ts
@@ -2,6 +2,6 @@ import { createClient } from '@supabase/supabase-js';
 
 // Conexão oficial do Escrita Tática ATIV
 const supabaseUrl = 'https://dbbzehyummpjyedxmsme.supabase.co';
-const supabaseAnonKey = 'sb_publishable_TGgaKlvlQpSHX42Tnjt4eg_tRuNngDM';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRiYnplaHl1bW1wanllZHhtc21lIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njg1Njc4MTMsImV4cCI6MjA4NDE0MzgxM30.sFH5-IG1ZmUh5OpXZrsg0aogm-Qt2CyF6eyrCaGAOlQ';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
